ce88635c347494108f1fcb0bcca8c4f51ddfb519
P-PMY <74496400+P-PMY@users.noreply.github.com>
2020-11-26 22:19:10 +0800

    Add files via upload

---
+++ paixu.h
@@ -0,0 +1,17 @@
+#ifndef mppx_h
+#define mppx_h
+#include<stdlib.h>
+#include<stdio.h>
+
+void BubbleSort(int arr[],int size);
+void Bubble(int arr[],int size);
+void InsertSort(int arr[],int size);
+void Insert(int arr[],int size);
+void Selectsort(int arr[],int size);
+void Select(int arr[],int size);
+int Partition(int arr[],int low,int high);
+void QSort(int arr[],int low,int high);
+void QuickSort(int arr[],int size);
+void Quick(int arr[],int size);
+
+#endif
